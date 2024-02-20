2f8e4f989ab9f071d431163419b8ad0b942981ad
danie odukwe <kingdanny295@gmail.com>
2024-02-20 15:45:21 +0100

    using useRef to pass reference

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import image from './assets/img.png'
+import { useRef, useEffect } from 'react'
+import pic from './assets/img.png'
 
 const App = () => {
+  let container = useRef(null)
+  let image = useRef(null)
+
   return (
     <section>
-      <div className="container">
+      <div ref={el=> {container = el}} className="container">
         <>
           <div className="img-container">
-            <img src={image} alt="people" />
+            <img ref={el => {image = el}} src={pic} alt="people" />
           </div>
         </>
       </div>
